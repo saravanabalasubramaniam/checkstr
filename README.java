6f02887c187dd847508be8375ccf4f2429ea2031
saravana <saravanasaru14313@gmail.com>
2019-02-11 22:53:20 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Checkstr
+{
+public static void main(String args[])
+	{
+	Scanner input=new Scanner(System.in);
+	String a=input.nextLine();
+	char c[]=a.toCharArray();
+	int flag=0;
+	for(int i=0;i<a.length();i++)
+	{
+		if((c[i]>='0')&&( c[i]<='9')||(c[i]>='a')&&( c[i]<='z')||(c[i]>='A')&&( c[i]<='Z'))
+		{
+			flag=1;		
+		}
+	}
+	if(flag==1)
+	{
+		System.out.println("yes");
+	}
+	else
+	{
+		System.out.println("no");
+	}
+  }
+}
